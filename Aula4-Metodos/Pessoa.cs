17d94e5d71233e5863ae08f737e6263adbd7cc2f
JonathasGomes <jonathas553@hotmail.com>
2022-04-12 19:49:04 -0300

    Aula 4: Métodos

---
+++ Aula4-Metodos/Pessoa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula4_Metodos
+{
+    class Pessoa
+    {
+        public void apresentar()
+        {
+            Console.WriteLine("Olá!!!");
+        }
+
+        public void apresentar(string nome)
+        {
+            Console.WriteLine("Ola " + nome);
+        }
+
+        public void apresentar(string nome, int idade)
+        {
+            Console.WriteLine("Olá " + nome + " você tem " + idade + " anos");
+        }
+    }
+}
---
+++ Aula4-Metodos/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Aula4_Metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa obj = new Pessoa();
+            obj.apresentar("jhon", 21);
+        }
+    }
+}
