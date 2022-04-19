30b6ff0f5cd874f1b2f6bc54f08b18fd0869a038
JonathasGomes <jonathas553@hotmail.com>
2022-04-19 09:10:51 -0300

    Aula5-Pratica

---
+++ Aula5-Pratica/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Aula5_Pratica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a = new Aluno();
+            a.nome = "Denis";
+            a.nota1 = 8;
+            a.nota2 = 6;
+            a.mensagem();
+
+        }
+    }
+}
--- Aula5-Pratica/obj/Debug/net5.0/Aula5-Pratica.AssemblyInfo.cs
+++ Aula5-Pratica/obj/Debug/net5.0/Aula5-Pratica.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Aula 4 - Metodos")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Aula5-Pratica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Aula 4 - Metodos")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Aula 4 - Metodos")]
+[assembly: System.Reflection.AssemblyProductAttribute("Aula5-Pratica")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Aula5-Pratica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
