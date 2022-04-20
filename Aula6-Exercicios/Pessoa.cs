39a1a5673b97f5e54847d76e0d51acefc2eba9f7
JonathasGomes <jonathas553@hotmail.com>
2022-04-20 08:59:45 -0300

    Aula 6 - Exercícios

---
+++ Aula6-Exercicios/Pessoa.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula6_Exercicios
+{
+    internal class Pessoa
+    {
+        public double peso, altura;
+
+        public double IMC()
+        {
+            return peso / (altura * altura);
+        }
+
+        public string situacao(double IMC)
+        {
+            string retorno;
+
+            if(IMC < 18.5)
+            {
+                retorno = "Abaixo do Peso";
+            }
+            else if(IMC < 25)
+            {
+                retorno = "Peso normal";
+            }
+            else if(IMC < 30)
+            {
+                retorno = "Acima do peso";
+            }
+            else if(IMC < 35)
+            {
+                retorno = "Obesidade I";
+            }
+            else if (IMC < 35)
+            {
+                retorno = "Obesidade I";
+            }
+            else if (IMC < 40)
+            {
+                retorno = "Obesidade II";
+            }
+            else
+            {
+                retorno = "Obesidade III";
+            }
+
+            return retorno; 
+        }
+
+        public void mensagem()
+        {
+            double Obtercalculo = IMC();
+            string Obtersituacao = situacao(Obtercalculo);
+
+            Console.WriteLine("Seu IMC é de " + Obtercalculo);
+            Console.WriteLine("Sua situacao é de " + Obtersituacao);
+        }
+    }
+}
---
+++ Aula6-Exercicios/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aula6_Exercicios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+
+            p.altura = 1.75;
+            p.peso = 65;
+            p.mensagem();
+        }
+    }
+}
