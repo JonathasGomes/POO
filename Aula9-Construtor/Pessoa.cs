c5c844fdde303936d032d2756d78267fb0fdda0f
JonathasGomes <jonathas553@hotmail.com>
2022-04-29 20:14:28 -0300

    Aula 9 - Construtor

---
+++ Aula9-Construtor/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula9_Construtor
+{
+    internal class Pessoa
+    {
+        public Pessoa()
+        {
+            Console.WriteLine("Construtor executado");
+        }
+
+        public Pessoa(string nome)
+        {
+            Console.WriteLine("Olá" + nome);
+        }
+    }
+}
---
+++ Aula9-Construtor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula9_Construtor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa("Jhon");
+        }
+    }
+}
