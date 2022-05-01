e78c0d3c30c71247b024c7286dd7598b46493ab2
JonathasGomes <jonathas553@hotmail.com>
2022-05-01 04:31:04 -0300

    Aula 10 - Get e Set

---
+++ Aula10-GetSet/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Aula10_GetSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+            p.Nome = "Jhon";
+            Console.WriteLine(p.Nome);
+        }
+    }
+}
