de5a934eac6aa3fce22c9632a27000e4b8a16109
JonathasGomes <jonathas553@hotmail.com>
2022-09-16 14:15:09 -0300

    this

---
+++ Aula11-This/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula11_This
+{
+    internal class Pessoa
+    {
+        private string nome = "Jhon";
+
+        public Pessoa(string nome)
+        {
+            Console.WriteLine(nome);
+            Console.WriteLine(this.nome);
+        }
+    }
+}
---
+++ Aula11-This/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula11_This
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa("Ralf");
+        }
+    }
+}
