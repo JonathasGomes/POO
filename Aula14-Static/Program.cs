dacfdf0197d9eed820aeec40755d23159e9f8a76
JonathasGomes <jonathas553@hotmail.com>
2022-09-21 18:33:01 -0300

    Static

---
+++ Aula14-Static/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula14_Static
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exemplo.soma(3, 7);
+        }
+    }
+}
