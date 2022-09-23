47d8d08900a423ec5e73eee03918bd7476abad0c
JonathasGomes <jonathas553@hotmail.com>
2022-09-23 08:23:25 -0300

    Interface

---
+++ Aula16-Interface/IPadrao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula16_Interface
+{
+    internal interface IPadrao
+    {
+        void somar(int n1, int n2);
+
+        void subtrair(int n1, int n2);
+    }
+}
---
+++ Aula16-Interface/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Aula16_Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculo c = new Calculo();
+            c.somar(15, 15);
+            c.subtrair(20, 5);
+        }
+    }
+}
